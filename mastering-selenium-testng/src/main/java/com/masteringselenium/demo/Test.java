e61b4701a68fae60497a0bd8e8cf892d9b1a77a1
raj1391 <rajasekar.j1391@gmail.com>
2018-10-02 16:42:30 +0530

    first

---
+++ mastering-selenium-testng/src/main/java/com/masteringselenium/demo/Test.java
@@ -0,0 +1,15 @@
+package com.masteringselenium.demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("test");
+		// TODO Auto-generated method stub
+
+	}
+	
+	 
+
+}
